00eabc51e4eeeccda9a401bfd72a9d1736dccc18
LalaineConde <161687040+LalaineConde@users.noreply.github.com>
2025-08-17 22:37:18 +0800

    add file
    added customer folder and product and contact files

---
+++ interfaces/customer/products.php
@@ -0,0 +1,41 @@
+<?php
+$page_title = 'Overview | Kesong Puti';
+require '../../connection.php'; 
+
+
+$toast_message = ''; // Initialize variable for toast message
+
+
+  
+
+// Close the connection
+mysqli_close($connection);
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Products</title>
+
+     <!-- BOOTSTRAP ICONS -->
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css"
+    />
+
+    <link rel="stylesheet" href="../../css/admin.css"/>
+
+</head>
+<body>
+    
+
+<!-- FUNCTIONS -->
+
+<!-- FUNCTIONS -->
+</body>
+</html>
+
+
